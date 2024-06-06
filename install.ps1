20f2679d0a2bb9c6c97a650e0cfa6dd8fa8edf52
M Zharfan Wiranata <zhargorist10@gmail.com>
2024-06-06 21:46:11 +0800

    update install req

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@ py -m venv .virtenv
 .\.virtenv\Scripts\Activate.ps1
 pip install .\radar_sdk\python_wheels\ifxradarsdk-3.5.0+8c595dbb-py3-none-win_amd64.whl
 
-pip install matplotlib
+pip install -r requirements.txt
