b5dd629f53dffd45bcdba31ea1532da432164bc9
M Zharfan Wiranata <zhargorist10@gmail.com>
2024-06-06 22:06:56 +0800

    new Updated

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 py -m venv .venv
 .\.venv\Scripts\Activate.ps1
-pip install .\radar_sdk\python_wheels\ifxradarsdk-3.5.0+8c595dbb-py3-none-win_amd64.whl
+pip install .\python_wheels\ifxradarsdk-3.5.0+8c595dbb-py3-none-win_amd64.whl
 
 pip install -r requirements.txt
